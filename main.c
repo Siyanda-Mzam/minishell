194f21bf34851e3a284404f348ed7b40f92260c3
myne <emjmeiring@gmail.com>
2016-07-07 23:46:33 +0200

    read_line added

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "minishell.h"
 #include <stdio.h>
+
 int		len(char *s)
 {
 	int		i;
@@ -32,17 +33,17 @@ void	parse_data(t_proutil *utils)
 	if (utils->line && *utils->line)
 	{
 		utils->cp_id = fork();
-		if(utils->cp_id == SUCCESS)
+		if(utils->cp_id == -1)
 		{
-			if (execve(utils->tokens[0], (utils->tokens + 1), utils->environ) == ERROR)
+			if (execvp(utils->tokens[0], (utils->tokens)) == -1)
 				erro_msg(utils->tokens[0], INVALID_COMMAND);
 			else if (utils->cp_id < 0)
 				erro_msg(utils->tokens[0], FAILED_FORK);
 			else
 			{
-				while (!(WIFEXITED(utils->status) && WIFSIGNALED(utils->status)))
+				while (!WIFEXITED(utils->status) || !WIFSIGNALED(utils->status))
 				{
-					utils->wp_id = waitpid(utils->cp_id, &utils->status, WUNTRACED | WNOHANG);
+					utils->wp_id = waitpid(utils->cp_id, &utils->status, WUNTRACED);
 				}
 			}
 		}
@@ -51,11 +52,54 @@ void	parse_data(t_proutil *utils)
 		return ;
 }
 
+void	buff_line(char *line, int length)
+{
+	char	*buff_mem;
+
+	buff_mem = ft_strnew(length);
+	buff_mem = ft_strcpy(buff_mem, line);
+	ft_memdel((void **)&line);
+	line = ft_strcpy(ft_strnew(length), buff_mem);
+}
+
+char	*get_line(void)
+{
+	char	*buf;
+	char	r;
+	int		b_size;
+	int		i;
+
+	i = 0;
+	b_size = 80;
+	buf = (char *)malloc(sizeof(char) * b_size);
+	while (42)
+	{
+		if (read(0, &r, 1) && r != 10)
+		{
+			if (r != '>')
+				*(buf + i) = r;
+		}
+		else
+		{
+			*(buf + i) = '\0';
+			return (buf);
+		}
+		i++;
+		if (i >= b_size)
+		{
+			b_size += b_size;
+			buff_line(buf, b_size);
+		}
+	}
+	return (NULL);
+}
+
 void	read_and_lex_data(t_proutil *util)
 {
-	read(0, &util->line, 7);
-	util->tokens = lex_line(&util->line);
-	parse_data(util);
+	util->line = get_line();
+	if (!ft_strcmp(util->line, "quit"))
+		exit(0);
+	printf("%s\n", util->line);
 }
 
 
@@ -68,12 +112,13 @@ int		main(void)
 {
 	t_proutil		tool_box;
 
-	tool_box.status = 1;
+	tool_box.status = -1;
 	//init_shell();
 	while (tool_box.status)
 	{
+		write(1, ">", 2);
 		read_and_lex_data(&tool_box);
-		parse_data(&tool_box);
+		//parse_data(util);
 	}
 	return (0);
 }
---
+++ read_line.c
@@ -0,0 +1,42 @@
+void	buff_line(char *line, int length)
+{
+	char	*buff_mem;
+
+	buff_mem = ft_strnew(length);
+	buff_mem = ft_strcpy(buff_mem, line);
+	ft_memdel((void **)&line);
+	line = ft_strcpy(ft_strnew(length), buff_mem);
+}
+
+char	*get_line(void)
+{
+	char	*buf;
+	char	r;
+	int		b_size;
+	int		i;
+
+	i = 0;
+	b_size = 80;
+	buf = (char *)malloc(sizeof(char) * b_size);
+	while (42)
+	{
+		if (read(0, &r, 1) && r != 10)
+		{
+			if (r != '>')
+				*(buf + i) = r;
+		}
+		else
+		{
+			*(buf + i) = '\0';
+			return (buf);
+		}
+		i++;
+		if (i >= b_size)
+		{
+			b_size += b_size;
+			buff_line(buf, b_size);
+		}
+	}
+	return (NULL);
+}
+
